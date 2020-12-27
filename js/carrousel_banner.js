e1119e5a3c4874e1c22986d17218d719089e8229
Sebastian Simarro <ssebastian210@gmail.com>
2020-12-27 20:21:30 -0300

    <javascritp> Actualizados links a Shop

--- js/carrousel_banner.js
+++ js/carrousel_banner.js
@@ -1,11 +1,11 @@
 // array de 2 niveles , carrousel cambia imagen y vinculo a pagina
 var grupoBanners = [];
 
-grupoBanners[0]=['https://organizatuplan.com.ar/productos/scrum-kanban-set-magnetico-premium/','images/banner0.png'];
-grupoBanners[1]=['https://organizatuplan.com.ar/productos/agiles-set-magnetico-standard/','images/banner1.png'];
-grupoBanners[2]=['https://organizatuplan.com.ar/productos/set-home-edition-standard/','images/banner2.png'];
-grupoBanners[3]=['https://organizatuplan.com.ar/productos/set-home-edition-premium/','images/banner3.png'];
-grupoBanners[4]=['https://organizatuplan.com.ar/productos/set-home-edition-basico/','images/banner4.png'];
+grupoBanners[0]=['https://articulo.mercadolibre.com.ar/MLA-900680458-set-magnetico-para-scrum-y-kanban-premium-plan-_JM','images/banner0.png'];
+grupoBanners[1]=['https://articulo.mercadolibre.com.ar/MLA-900682051-combo-pizarra-blanca-magnetica-60x45-set-premium-_JM','images/banner1.png'];
+grupoBanners[2]=['https://articulo.mercadolibre.com.ar/MLA-900678482-planner-magnetico-home-edition-standard-plan-_JM','images/banner2.png'];
+grupoBanners[3]=['https://articulo.mercadolibre.com.ar/MLA-900680453-planner-magnetico-home-edition-premium-plan-_JM','images/banner3.png'];
+grupoBanners[4]=['https://articulo.mercadolibre.com.ar/MLA-900681227-combo-pizarra-blanca-magnetica-60x45-set-basico-_JM','images/banner4.png'];
 
 var links = document.getElementById('links');
 var imagenes = document.getElementById('imagen');
